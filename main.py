97874f71316d7862f1d3abc301624518fec809a7
monicaglory <124677775+monicaglory@users.noreply.github.com>
2023-02-27 17:18:36 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+first_scorer='Ruud Gullit'
+second_scorer='Marco Van Basten'
+goal_0=32
+goal_1=54
+scorers=first_scorer + " " + str(goal_0), second_scorer + " " + str(goal_1)
+report=f'Ruud Gullit scored in the {goal_0}nd minute \n Marco Van Basten scored in the {goal_1}th minute'
+print(report)
+player='Jan. Wouters'
+print(player[:3])
+first_name=player[:3]
+last_name=player[4:11]
+print(last_name,len(last_name))
+name_short=(player[0]+ player[3:11])
+print(name_short)
+chant='Ruud!\tRuud!\tRuud!'
+print(chant)
